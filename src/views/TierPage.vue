fb4791efec9b4e9b42dea18154f24ebbd2e90489
Jimmy Iu <jimmy@arch-partnership.com>
2021-03-02 02:55:39 +0800

    fix: test rotate issue

---
+++ src/views/TierPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-alert dense dismissible outlined type="info">Organize heros</v-alert>
+    <hero-form-card />
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { HeroFormCard } from '@/components';
+import { mapGetters } from 'vuex';
+
+@Component({
+  name: 'tier-page',
+  components: { HeroFormCard },
+  computed: { ...mapGetters(['gears', 'getGear']) }
+})
+export default class TierPage extends Vue {}
+</script>
